85c840b53e794baac7c8d4c41e66c0d09f32ab36
nho1145 <henrylhy2022@gmail.com>
2026-04-14 12:44:59 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// 1. Find the button in the HTML using its ID
+const button = document.getElementById('myButton');
+
+// 2. Listen for a "click" event
+button.addEventListener('click', function() {
+    // 3. This is what happens when the click occurs
+    alert("The Button has been activated! 🤖✨");
+});
